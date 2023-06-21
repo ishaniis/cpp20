701d3f59f4fa7e7a29836e3cb2c2f655e09e5809
Ishan Sharma <sharmaishan1508@gmail.com>
2023-06-21 12:05:28 -0400

    Fibonacci Sequence

--- tut18.cpp
+++ tut18.cpp
@@ -2,8 +2,35 @@
 #include<iostream>
 using namespace std;
 
+int fibterm(int n)
+{   
+    if (n==0){
+        return 0;
+    }
+    else if (n<=2 & n!= 0){
+        return 1;
+    }
+    return fibterm(n-2) + fibterm(n-1);
+}
+
+bool printfibseries(int n){
+
+    int firstterm = 0;
+    int secondterm = 0;
+    int finalterm = 0;
+
+    for (int i = 0; i <= n ; i++){
+        cout<<fibterm(i)<<",";
+    }
+    cout<<"."<<endl;
+    return true;
+}
+
 int main(){
     
+    int num = 10;
+    cout<<"Fib term for"<<num<<" term is"<<fibterm(num)<<endl;
+    cout<<"Print the series"<<printfibseries(num);
 
     return 0;
 }
