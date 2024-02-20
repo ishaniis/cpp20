567e79bb17139432d53808ebd05feadde48673b8
Ishan Sharma <sharmaishan1508@gmail.com>
2024-02-20 18:21:48 -0500

    Concept of Pointer discussed using the new keyword Usage of pointers in the class and it's objects.
    Also, learnt the Concept of developing temporary pointer to store initial information.

---
+++ tut36.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class shopItem
+{
+private:
+    int id;
+    float price;
+public:
+    void setData(int a , float b){
+        id = a;
+        price = b;
+        cout<<"The price has been set"<<endl;
+    }
+    
+    void getData(){
+        cout<<"The id of the listed ShopItem is:"<<id<<endl;
+        cout<<"The price of the listed ShopItem is:"<<price<<endl;
+    }
+    
+};
+
+
+int main(){
+    
+    cout<<"Demonstrating using pointers in Classes"<<endl;
+    cout<<"Calling the object ShopItem"<<endl;
+
+    shopItem *ptr = new shopItem[3];
+    shopItem *ptr_temp = ptr;
+    
+    //Input Variables and setting up variables
+    int p;
+    float q;
+    int size = 3;
+
+
+    //Invocation using setData function of the class
+    for (int i = 0; i < size; i++)
+    {
+        cout<<"Enter the id of the listed ShopItem"<<endl;
+        cin>>p;
+        cout<<"Enter the price of the listed ShopItem"<<endl;
+        cin>>q;
+        ptr->setData(p,q);
+        ptr++;        
+    }
+
+    //Now using this loop printout the data
+    for (int i = 0; i < size; i++)
+    {
+        cout<<"Item Number"<<endl;
+        ptr_temp->getData();
+        ptr_temp++;
+    }
+    
+    
+    
+    return 0;
+}
