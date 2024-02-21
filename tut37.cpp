2e348ca25b7db1c7a39d423d54ac09503832fc6c
Ishan Sharma <sharmaishan1508@gmail.com>
2024-02-21 10:42:54 -0500

    Just completed this pointer implementation in this code.

--- tut37.cpp
+++ tut37.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
+using namespace std;
 
 class base1
 {
-private:
-    /* data */
 public:
-    base1(/* args */);
-    ~base1();
+    int b;
+    //int a, b;
+    base1(){};
+//
+// base1 & base1(intb) -> b.getdata()
+    // base1(int b){
+    //     // b = base1;
+    //     // base(int b1)
+    //     // In the below statement we're implicitly referring to the pointer of b,
+    //     //rather than doing it explicitly. 
+    //     // b = b1; 
+    //     // cout<<"The setvalues are:"<<b<<endl;
+        
+    //     //Implicitly referring to the pointer b, which is an object of class b1
+    //     this->b = b;
+    // }
+
+    void setData(int b){
+        this-> b = b;
+    }
+    void getData(void){
+        cout<<"The setvalues are:"<<b<<endl;
+    }
+
+    //Making setdata(3).getdata() -> possible (As this is making setdata an object to call member functions)
+    base1 & setdata(int b){
+        this-> b = b;
+        return *this;
+    }
 };
 
-base1::base1(/* args */)
+int main()
 {
-}
+    base1 a;
+    // a.setData(3);
+    // a.getData();
 
-base1::~base1()
-{
+    a.setdata(5).getData();
+
+    return 0;
 }
