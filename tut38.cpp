2c9bb8524803cf3a08a08261c037fb07c2b268ca
Ishan Sharma <sharmaishan1508@gmail.com>
2024-02-21 11:36:23 -0500

    Concept of Polymorphism and its theory discussed. - Types of Polymorphism is also discussed in this section.

---
+++ tut38.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    cout<<"Concept of Polymorphism"<<endl;
+    //Concept of Polymorphism
+    // One name -> multiple forms
+
+    //Types of Polymorphism
+    //1. Compile-time Polymorphism
+    // It can be achieved by these following:
+    //  1.1 Operator Overloading
+    //  1.2 Function Overloading
+
+    //2. Run-time Polymorphism
+    //  It can be achieved using
+    // 2.1 Virtual Function
+
+    
+
+    return 0;
+}
