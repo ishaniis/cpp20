54246a5ee86d704a430b6f7ec88f28888e6b1348
Ishan Sharma <sharmaishan1508@gmail.com>
2024-03-11 18:49:15 -0400

    Just some concepts of different types of containers, is being discussed: - Sequence containers - Associative containers - Derived containers
    What order Stack follows?
    
    What order Queue follows ?
    
    Different operation and their respective speed to accomplish that given operation.

---
+++ tut43.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*
+    In this tut43, we would explore different types of containers:
+
+    1. Sequential Containers:
+    1.1 Stores data in a linear fashion
+    1.2 Elements to be added either added in the beginning or in the ending 
+    1.3 Example: Vector
+    1.3.1 Vectors:
+    1.3.1.1 Faster Random Access
+    1.3.1.2 Slower Insertion and Deletion
+    1.3.1.3 Faster Insertion (At End)
+
+    2. Associative Containers:
+    2.1 Associative containers are designed such that enhances the way accessing some elements in that container
+    2.2 Fastest and user friendly way to reach out to some elements in a given container. 
+    2.3 Example: Set, Multiset, Map, Multimap etc. 
+    2.4 In asociative containers, except random access, all the operations are FASTER, but we cannout jump into conclusion as to which operation is faster or slower. 
+    
+
+    3. Derived Containers:
+    3.1 Derived from either the sequence or the associative containers. 
+    3.2 Real Life modelling 
+    3.3 Examples: Stack, Queue, Priority Queue etc. 
+
+    3.3.1 Stack: LIFO (Last In, First Out)
+    3.3.2 Queue: FIFO (First In, First Out)
+    3.3.3 Lists:
+    3.3.3.1 Random Access is Slow
+    3.3.3.2 Insertion and deletion at any position is relatively faster, because they only use pointers
+    
+    */
+    return 0;
+}
